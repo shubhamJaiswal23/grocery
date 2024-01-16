75c6172fe740c99dc70edee01f608bd03b61f29c
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2024-01-16 14:27:31 +0530

    grocery bud project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,63 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { ToastContainer, toast } from 'react-toastify';
+import Form from './Form';
+import Items from './Items';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    list = JSON.parse(localStorage.getItem('list'));
+  } else {
+    list = [];
+  }
+  return list;
+};
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+};
+
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+  const [items, setItems] = useState(getLocalStorage());
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid()
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
+    // toast.success('item added successfully');
+  };
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    setLocalStorage(newItems);
+  };
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
+  };
+
+  return (
+    <section className="section-center">
+      <ToastContainer position="top-center" />
+      <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
+    </section>
+  );
 };
 
 export default App;
